8375237b4de3d79274c6a1bbca6b7ad7995b01ff
Manu-Madhu <manum66466@gmail.com>
2025-06-07 11:09:14 +0530

    commit

--- src/components/product_view/ProductDetailedView.tsx
+++ src/components/product_view/ProductDetailedView.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 const ProductDetailedView = ({ id }: { id: string }) => {
 
     const Product = ProductData.find((item)=> item.seo_title === id);
+    console.log(Product)
     return (
         <div>
 
