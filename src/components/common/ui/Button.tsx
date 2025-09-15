9cce42d757f3f37e48d677505772360eec93ef25
Manu-Madhu <manum66466@gmail.com>
2025-09-15 14:00:12 +0530

    update the button part

--- src/components/common/ui/Button.tsx
+++ src/components/common/ui/Button.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 
 const Button = ({ title, link, className, onClick }: ButtonProps) => {
   return (
-    <Link href={link || ""} title={title || "Button"} className={cs('bg-button p-1.5 md:p-2 uppercase px-5 text-xs md:text-sm md:px-5', className)}>
-      <button onClick={onClick} className='cursor-pointer text-nowrap'>
+    <Link href={link || ""} title={title || "Button"} >
+      <button onClick={onClick} className={cs('bg-button p-1.5 md:p-2 uppercase px-5 text-xs md:text-sm md:px-5 cursor-pointer text-nowrap', className)}>
         {title}
       </button>
     </Link>
